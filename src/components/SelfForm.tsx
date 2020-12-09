3287dc4baeb9cedee5f01ab9848e792acb2a8f99
Kasparas Masiukas <kmasiukas@gmail.com>
2020-12-09 11:14:24 +0000

    Small styling updates

--- src/components/SelfForm.tsx
+++ src/components/SelfForm.tsx
@@ -10,8 +10,8 @@ const SelfForm: React.FC = () => (
   <div>
     <Paper elevation={3} className="content-paper instructions-paper">
       <div className="instructions-container">
-        <Typography variant="h3">Savęs vertinimas</Typography>
-        <Typography className="justify" variant="body1">Atlikite savęs vertinimo testą. Gavus nuorodą nusiųskite savo draugams, kurių nuomonę apie save norėtumėte išgirsti ir sužinokite, kurios Jūsų savybės yra matomos kitiems, o kurios nežinomos. Nemokamai. Be jokios registracijos.        
+        <Typography style={{ marginBottom: 10 }} variant="h3">Savęs vertinimas</Typography>
+        <Typography className="justify" variant="body1">Atlikite savęs vertinimo testą. Gavus nuorodą nusiųskite savo draugams, kurių nuomonę apie save norėtumėte išgirsti ir sužinokite, kurios Jūsų savybės yra matomos kitiems, o kurios nežinomos. Nemokamai. Be jokios registracijos.
         </Typography>
       </div>
     </Paper>
