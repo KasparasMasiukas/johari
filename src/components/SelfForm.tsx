f38f8fc868ab1d5ec221b8ed08f58206b166a8f4
Kamelia Zhelyazkova <KameliaZhelyazkova@users.noreply.github.com>
2020-12-09 11:09:49 +0000

    Added updated introductions description.
    Added some imports and project description.

--- src/components/SelfForm.tsx
+++ src/components/SelfForm.tsx
@@ -1,4 +1,6 @@
 import React from 'react';
+import '../styles/SelfForm.scss';
+import { Paper, Typography } from '@material-ui/core';
 import DetailsForm from './DetailsForm';
 import TraitsGrid from './TraitsGrid';
 import FinishButton from './FinishButton';
@@ -6,6 +8,13 @@ import { MAX_SELECTIONS, MIN_SELECTIONS } from '../context/globals';
 
 const SelfForm: React.FC = () => (
   <div>
+    <Paper elevation={3} className="content-paper instructions-paper">
+      <div className="instructions-container">
+        <Typography variant="h3">Savęs vertinimas</Typography>
+        <Typography className="justify" variant="body1">Atlikite savęs vertinimo testą. Gavus nuorodą nusiųskite savo draugams, kurių nuomonę apie save norėtumėte išgirsti ir sužinokite, kurios Jūsų savybės yra matomos kitiems, o kurios nežinomos. Nemokamai. Be jokios registracijos.        
+        </Typography>
+      </div>
+    </Paper>
     <DetailsForm />
     <TraitsGrid title={`Aš esu... (pasirinkite nuo ${MIN_SELECTIONS} iki ${MAX_SELECTIONS} savybių)`} selectable />
     <FinishButton recipientId="" />
